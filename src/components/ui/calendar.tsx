b202dc4edbd28f8d0c63d562b7c390b2f5534426
digitnetx <digitnetx@gmail.com>
2026-08-19 03:09:19 +0300

    Fix calendar for react-day-picker v9

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { ChevronLeft, ChevronRight } from "lucide-react"
+import { ChevronDown, ChevronLeft, ChevronRight, ChevronUp } from "lucide-react"
 import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
@@ -54,12 +54,21 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("h-4 w-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation, className, size, ...props }) => {
+          const iconProps = { className: cn("h-4 w-4", className), size, ...props }
+
+          switch (orientation) {
+            case "left":
+              return <ChevronLeft {...iconProps} />
+            case "right":
+              return <ChevronRight {...iconProps} />
+            case "up":
+              return <ChevronUp {...iconProps} />
+            case "down":
+            default:
+              return <ChevronDown {...iconProps} />
+          }
+        },
       }}
       {...props}
     />
